d30349001fd9b091c6c071214c9ea5bc6cb52996
Josh <josh@josh-linux.(none)>
2014-11-09 14:08:09 +1300

    Detect when audo-programs has been updated and disply changelog (SAB, SB, CP, HP versions)

--- default.py
+++ default.py
@@ -3,6 +3,9 @@ import xbmc
 import xbmcaddon
 import os
 
+import xbmcvfs
+import utils
+
 __scriptname__ = "audo"
 __author__     = "lsellens"
 __url__        = "http://code.google.com/p/repository-lsellens/"
@@ -18,7 +21,20 @@ except:
     xbmc.log('AUDO: Could not detect service addon:', level=xbmc.LOGERROR)
     exit()
 
+#Audo Programs addon info.
+__programs__ = xbmc.translatePath(xbmcaddon.Addon(id='script.module.audo-programs').getAddonInfo('path'))
+
 if __name__ == '__main__':
+
+    # detect update of audo-programs and restart services
+    if not xbmcvfs.exists(xbmc.translatePath(__programs__ + '/.changelog')):
+        try:
+            utils.TextBox()
+            #Touch changelog
+            open(__programs__ + '/.changelog', 'a').close()
+        except Exception, e:
+            xbmc.log('InternetPVR: Could not retrieve audo changelog:', level=xbmc.LOGERROR)
+            xbmc.log(str(e), level=xbmc.LOGERROR)
 
     # Shutdown audo
     os.system("kill `ps | grep -E 'python.*script.module.audo' | awk '{print $1}'`")
