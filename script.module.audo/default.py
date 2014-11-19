14a8c57b6918e6a9b940704123b1e574da86213b
lsellens <lsellens@gmail.com>
2014-11-18 19:26:02 -0600

    temporarily remove changelog display in menu settings. utils is not included in OE

--- script.module.audo/default.py
+++ script.module.audo/default.py
@@ -2,8 +2,6 @@
 import xbmc
 import xbmcaddon
 import os
-import xbmcvfs
-import utils
 
 __scriptname__ = "audo"
 __author__     = "lsellens"
@@ -24,16 +22,6 @@ except:
 __programs__ = xbmc.translatePath(xbmcaddon.Addon(id='script.module.audo-programs').getAddonInfo('path'))
 
 if __name__ == '__main__':
-
-    # detect update of audo-programs and restart services
-    if not xbmcvfs.exists(xbmc.translatePath(__programs__ + '/.changelog')):
-        try:
-            utils.TextBox()
-            #Touch changelog
-            open(__programs__ + '/.changelog', 'a').close()
-        except Exception, e:
-            xbmc.log('AUDO: Could not retrieve audo changelog:', level=xbmc.LOGERROR)
-            xbmc.log(str(e), level=xbmc.LOGERROR)
 
     # Shutdown audo
     os.system("kill `ps | grep -E 'python.*script.module.audo' | awk '{print $1}'`")
