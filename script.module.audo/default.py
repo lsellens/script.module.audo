3a07ca21ce3261c8dba20bdda47a86165486ed3e
lsellens <lsellens@gmail.com>
2014-11-29 19:08:43 -0600

    reworked changlog display

--- script.module.audo/default.py
+++ script.module.audo/default.py
@@ -1,9 +1,8 @@
 #
+from os import system
 import xbmc
 import xbmcaddon
-import os
 import xbmcvfs
-import utils
 
 __scriptname__ = "audo"
 __author__     = "lsellens"
@@ -16,27 +15,14 @@ try:
     __addonpath__  = __addon__.getAddonInfo('path')
     __start__      = xbmc.translatePath(__addonpath__ + '/resources/audo.py')
 except:
-    xbmc.executebuiltin("XBMC.Notification('audo', 'Install audo service!', '30000', %s)" % ( __selfpath__ + '/icon.png'))
+    xbmc.executebuiltin("XBMC.Notification('audo', 'Install audo service!', '5000', %s)" % ( __selfpath__ + '/icon.png'))
     xbmc.log('AUDO: Could not detect service addon:', level=xbmc.LOGERROR)
     exit()
 
-#Audo Programs addon info.
-__programs__ = xbmc.translatePath(xbmcaddon.Addon(id='script.module.audo-programs').getAddonInfo('path'))
-
 if __name__ == '__main__':
 
-    # detect update of audo-programs and restart services
-    if not xbmcvfs.exists(xbmc.translatePath(__programs__ + '/.changelog')):
-        try:
-            utils.TextBox()
-            #Touch changelog
-            open(__programs__ + '/.changelog', 'a').close()
-        except Exception, e:
-            xbmc.log('AUDO: Could not retrieve audo changelog:', level=xbmc.LOGERROR)
-            xbmc.log(str(e), level=xbmc.LOGERROR)
-
     # Shutdown audo
-    os.system("kill `ps | grep -E 'python.*script.module.audo' | awk '{print $1}'`")
+    system("kill `ps | grep -E 'python.*script.module.audo' | awk '{print $1}'`")
 
     #Open settings dialog
     __addon__.openSettings()
