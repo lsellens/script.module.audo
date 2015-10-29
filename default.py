e73dde87aece3816eb25eabc58d9634f70b1b18a
lsellens <lsellens@gmail.com>
2015-10-29 09:48:02 -0500

    modified method to restart after settings changed

---
+++ default.py
@@ -0,0 +1,28 @@
+#
+import xbmc
+import xbmcaddon
+
+__scriptname__ = "audo"
+__author__     = "lsellens"
+__url__        = "http://lsellens.openelec.tv"
+__icon__   = xbmcaddon.Addon(id='script.module.audo').getAddonInfo('icon')
+
+#try to get service addon info or send notification to install it.
+try:
+    __addon__      = xbmcaddon.Addon(id='script.service.audo')
+    __addonpath__  = __addon__.getAddonInfo('path')
+except:
+    xbmc.executebuiltin("XBMC.Notification(audo, Install audo service!, 15000, %s)" % ( __icon__))
+    xbmc.log('AUDO: Could not detect service addon:', level=xbmc.LOGERROR)
+    exit()
+
+if __name__ == '__main__':
+    
+    # Shutdown audo
+    __addon__.setSetting(id='SHUTDOWN', value='true')
+    
+    # Open settings dialog
+    __addon__.openSettings()
+    
+    # Restart audo
+    __addon__.setSetting(id='SHUTDOWN', value='false')
